b799b9bedf0be0b833dab2190bd8b4d338740911
dlarej <liu.jerald@gmail.com>
2015-10-03 21:55:30 -0400

    Added simple test

--- test.c
+++ test.c
@@ -5,7 +5,24 @@
 int main(int argc, char * argv[]) 
 {
   // Test sleeptable
+  int pid;
+  int parent = getpid();
+  pid = fork();
+  if (pid == 0) {
+    // Child Process
+    printf(1, "Child id: %d\n", pid);
+    kill(parent);
+  } else if (pid > 0) {
+    // Parent Process
+    printf(1, "Parent id: %d\n", pid);
+    wait();
+    printf(1, "Parent: Child done executing.\n");
+  } else {
+    // Error, failed to fork()
+  }
   
-  printf(1, "%d\n", nice(1));
+  
+  
+  //printf(1, "%d\n", nice(1));
   exit(); 
 }
