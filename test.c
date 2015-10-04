522225bc85b2ca6f97f3984c85c018be5ed351aa
dlarej <liu.jerald@gmail.com>
2015-10-03 22:55:03 -0400

    Added to README-PROCESSES. Edited test.c

--- test.c
+++ test.c
@@ -10,19 +10,17 @@ int main(int argc, char * argv[])
   pid = fork();
   if (pid == 0) {
     // Child Process
-    printf(1, "Child id: %d\n", pid);
+    // Kill parent
+    kill(parent);
     kill(parent);
   } else if (pid > 0) {
     // Parent Process
-    printf(1, "Parent id: %d\n", pid);
     wait();
-    printf(1, "Parent: Child done executing.\n");
+    printf(1, "This should not have printed\n");
   } else {
     // Error, failed to fork()
   }
-  
-  
-  
+   
   //printf(1, "%d\n", nice(1));
   exit(); 
 }
