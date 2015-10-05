1b4163d0d67e4e6032e86f17f78b6f017d2a2441
Daniel Swann <dswann5@jhu.edu>
2015-10-04 22:59:36 -0400

    Priority calculations

--- param.h
+++ param.h
@@ -1,5 +1,7 @@
 #define NPROC        64  // maximum number of processes
 #define NPRIORITIES   5  // number of total possible priorities
+#define LOWEST_PRIORITY     (NPRIORITIES/2)*-1 // Lowest priority, defaults to -2
+#define HIGHEST_PRIORITY    (NPRIORITIES/2) // Highest priority, defaults to 2
 #define KSTACKSIZE 4096  // size of per-process kernel stack
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
--- sysproc.c
+++ sysproc.c
@@ -98,8 +98,6 @@ sys_nice(void)
     int increment;
     if(argint(0, &increment) < 0)
         return -1;
- 
-    //int pid = proc->pid;
- 
-    return -1;
+
+    return change_priority(increment);
 }
