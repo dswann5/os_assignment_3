6d090afd9d7e346b4e63bac8b0979ccc95e6a72f
Daniel Swann <dswann5@jhu.edu>
2015-10-06 21:32:58 -0400

    Refactoring and comments

--- test.c
+++ test.c
@@ -1,18 +1,12 @@
+// Daniel Swann and Jerald Liu
+// Operating Systems Fall 2015
+// Assignment 3
+// Test file, please ignore
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "param.h"
-//Only takes positive (and zero) exponents
-int pow(int base, int exponent) {
-    int sum = base;
-    if (exponent == 0)
-	return 1;
-    int i;
-    for (i=1;i<exponent;i++) {
-	sum*=base;
-    }
-    return sum;
-}
 
 int main(int argc, char * argv[]) 
 {
@@ -20,25 +14,25 @@ int main(int argc, char * argv[])
   int pid;
   int parent = getpid();
   int i;
-  for (i=0;i<7;i++) {
-    printf(1, "Nice %d returned %d\n", 1, nice(1));
+  for (i=0;i<1;i++) {
+    printf(1, "Nice %d returned %d\n", 1, nice(2));
     pid = fork();
-    printf(1, "Nice %d returned %d\n", 3, nice(3));
-    printf(1, "Nice %d returned %d\n", -1, nice(-1));
+    printf(1, "Nice %d returned %d\n", 3, nice(-4));
+    printf(1, "Nice %d returned %d\n", -1, nice(2));
     if (pid == 0) {
       // Child Process
       // Kill parent
       printf(1, "Child process\n");
-      printf(1, "%d\n", kill(parent));
+      printf(1, "Kill returned %d\n", kill(parent));
       kill(parent);
     } else if (pid > 0) {
       // Parent Process
       wait();
-      printf(1, "This should not have printed\n");
     } else {
       // Error, failed to fork()
+      printf(1, "Error: fork failure");
     }
 
-  }   
+  }  
   exit(); 
 }
